b19dd9cec2715ecde33bef80e9640f2e7a153a42
Keith Cirkel <keithamus@users.noreply.github.com>
2022-02-07 18:24:41 +0000

    initial project setup
    Co-authored-by: Kristján Oddsson <koddsson@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+const baseSupport =
+  typeof Blob === 'function' &&
+  typeof PerformanceObserver === 'function' &&
+  typeof Intl === 'object' &&
+  typeof MutationObserver === 'function' &&
+  typeof URLSearchParams === 'function' &&
+  typeof WebSocket === 'function' &&
+  typeof IntersectionObserver === 'function' &&
+  typeof queueMicrotask === 'function' &&
+  typeof TextEncoder === 'function' &&
+  typeof TextDecoder === 'function' &&
+  typeof customElements === 'object' &&
+  typeof HTMLDetailsElement === 'function' &&
+  typeof AbortController === 'function' &&
+  typeof AbortSignal === 'function' &&
+  'entries' in FormData.prototype &&
+  'toggleAttribute' in Element.prototype &&
+  'replaceChildren' in Element.prototype &&
+  // ES2019
+  'fromEntries' in Object &&
+  'flatMap' in Array.prototype &&
+  'trimEnd' in String.prototype &&
+  // ES2020
+  'allSettled' in Promise &&
+  'matchAll' in String.prototype &&
+  // ES2021
+  'replaceAll' in String.prototype &&
+  // 'any' in Promise && // Polyfilled
+  // ES2022
+  // 'at' in String.prototype && // Polyfilled
+  // 'at' in Array.prototype && // Polyfilled
+  true
+
+export function isSupported() {
+  return baseSupport
+}
+
+export function isPolyfilled() {
+  return false
+}
+
+export function apply() {
+  return
+}
---
+++ test/index.js
@@ -0,0 +1,11 @@
+import {apply, isPolyfilled, isSupported} from '../lib/index.js'
+
+describe('abortSignalAbort', () => {
+  it('has standard isSupported, isPolyfilled, apply API', () => {
+    expect(isSupported).to.be.a('function')
+    expect(isPolyfilled).to.be.a('function')
+    expect(apply).to.be.a('function')
+    expect(isSupported()).to.be.a('boolean')
+    expect(isPolyfilled()).to.equal(false)
+  })
+})
