ee56483410a1b8bedb31940dd8683435aa03eab1
Keith Cirkel <keithamus@users.noreply.github.com>
2024-01-29 18:02:02 +0000

    Remove a whole bunch of polyfills
    BREAKING CHANGE: This removes a lot of polyfills, and pushes our minimum base support up

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,11 @@
-import * as abortSignalAbort from './abortsignal-abort.js'
 import * as abortSignalTimeout from './abortsignal-timeout.js'
-import * as arrayAt from './arraylike-at.js'
 import * as clipboardItem from './clipboarditem.js'
-import * as cryptoRandomUUID from './crypto-randomuuid.js'
 import * as elementCheckVisibility from './element-checkvisibility.js'
-import * as eventAbortSignal from './event-abortsignal.js'
 import * as navigatorClipboard from './navigator-clipboard.js'
-import * as formRequestSubmit from './form-requestsubmit.js'
-import * as objectHasOwn from './object-hasown.js'
 import * as requestIdleCallback from './requestidlecallback.js'
-import * as arrayFindLast from './array-findlast.js'
-import * as arrayFindLastIndex from './array-findlastindex.js'
 
 export const baseSupport =
-  typeof Blob === 'function' &&
-  typeof PerformanceObserver === 'function' &&
-  typeof Intl === 'object' &&
-  typeof MutationObserver === 'function' &&
-  typeof URLSearchParams === 'function' &&
-  typeof WebSocket === 'function' &&
-  typeof IntersectionObserver === 'function' &&
-  typeof queueMicrotask === 'function' &&
-  typeof TextEncoder === 'function' &&
-  typeof TextDecoder === 'function' &&
-  typeof customElements === 'object' &&
-  typeof HTMLDetailsElement === 'function' &&
-  typeof AbortController === 'function' &&
-  typeof AbortSignal === 'function' &&
   typeof globalThis === 'object' &&
-  'entries' in FormData.prototype &&
-  'toggleAttribute' in Element.prototype &&
   // ES2019
   'fromEntries' in Object &&
   'flatMap' in Array.prototype &&
@@ -41,32 +17,29 @@ export const baseSupport =
   'replaceAll' in String.prototype &&
   'any' in Promise &&
   // ES2022
-  // 'at' in String.prototype && // Polyfilled
-  // 'at' in Array.prototype && // Polyfilled
-  // 'hasOwn' in Object && // Polyfilled
+  'at' in String.prototype &&
+  'at' in Array.prototype &&
+  'hasOwn' in Object &&
   // ESNext
-  // 'abort' in AbortSignal && // Polyfilled
+  'abort' in AbortSignal &&
   // 'timeout' in AbortSignal && // Polyfilled
+  // DOM / HTML and other specs
+  typeof queueMicrotask === 'function' &&
+  typeof HTMLDialogElement === 'function' &&
   typeof AggregateError === 'function' &&
-  // 'randomUUID' in crypto && // Polyfilled
+  typeof BroadcastChannel === 'function' &&
+  'randomUUID' in crypto &&
   'replaceChildren' in Element.prototype &&
+  'requestSubmit' in HTMLFormElement.prototype &&
   // 'requestIdleCallback' in window && // Polyfilled
   true
 
 export const polyfills = {
-  abortSignalAbort,
   abortSignalTimeout,
-  arrayAt,
   clipboardItem,
-  cryptoRandomUUID,
   elementCheckVisibility,
-  eventAbortSignal,
   navigatorClipboard,
-  formRequestSubmit,
-  objectHasOwn,
   requestIdleCallback,
-  arrayFindLast,
-  arrayFindLastIndex,
 }
 
 export function isSupported() {
